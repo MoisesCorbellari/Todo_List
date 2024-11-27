ac10723135d06556589a7cfce8353483f9e5fb12
Moises Corbellari <moisescorbellari1@outlook.com>
2024-11-27 01:48:11 -0300

    add and changed file

--- shared/database.py
+++ shared/database.py
@@ -1,7 +1,10 @@
+from dotenv import load_dotenv
 import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+
+load_dotenv()
 
 SQLALCHEMY_DATABASE_URL = os.getenv("SQLALCHEMY_DATABASE_URL")
 
