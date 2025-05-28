53da4556bf18d49053c7237add23e217824f83e0
Moises Corbellari <moisescorbellari1@outlook.com>
2025-05-27 21:21:40 -0300

    chore: ajustando mensagem em exceptions_handler

--- BackEnd_ToDoList/shared/exceptions_handler.py
+++ BackEnd_ToDoList/shared/exceptions_handler.py
@@ -6,5 +6,5 @@ from shared.exception import NotFound
 async def not_found_exception_handler(request: Request, exc: NotFound):
     return JSONResponse(
         status_code=404,
-        content={"message": f"Oops! {exc.name}Tarefa não encontrada!"},
+        content={"message": f"Oops! {exc.name}Atividade não encontrada!"},
     )
